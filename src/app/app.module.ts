961fda7d57d251d0a4c323275e49e852c7868328
Alan Sousa <alansousa.si@gmail.com>
2023-05-25 08:49:07 -0300

    Criação de 2 novos componentes para atividade 07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopoComponent,
+    RodapeComponent,
   ],
   imports: [
     BrowserModule,
